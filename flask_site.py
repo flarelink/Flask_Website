f618c5fc6e30b2b4a19541e4a7a8aa878fa14b8e
hxs7174 <hxs7174@g.rit.edu>
2019-11-29 22:07:35 -0500

    added navbar and simple design

--- flask_site.py
+++ flask_site.py
@@ -1,17 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+# dummy variable posts
+posts = [
+    {
+        'author': 'Humza Syed',
+        'title' : 'Test Blog - Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 29, 2019'
+    },
+    
+    {
+        'author': 'H Syed',
+        'title' : 'Test Blog - Post 2',
+        'content': 'First post content',
+        'date_posted': 'November 29, 2019'
+    }
+]
+
+# home page
 @app.route('/')
 @app.route('/home')
 def home():
-    s = '<h1> Home! </h1>'
-    return s
+    return render_template('home.html', posts=posts)
 
+# about page
 @app.route('/about')
 def about():
-    s = '<h1> About Page </h1>'
-    return s
+    return render_template('about.html', title='About')
 
+# debug mode to easily check new website
 if __name__ == '__main__':
     app.run(debug=True)
 
