aa9671dde0f16fb88da345caca8cbace5e89741c
hxs7174 <hxs7174@g.rit.edu>
2019-11-30 00:03:29 -0500

    added register/login forms

--- flask_site.py
+++ flask_site.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+# secret key to protect against attacks
+app.config['SECRET_KEY'] = '6db96e6441e9bc1ebf126dccb395aed0'
 
 # dummy variable posts
 posts = [
@@ -22,12 +26,39 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', title='Home', posts=posts)
 
 # about page
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+# resume page
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title='Resume')
+
+# registration page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')  # f portion is for python >= 3.6
+        return redirect(url_for('home'))  # redirect to home page
+    return render_template('register.html', title='Register', form=form)
+
+# login page
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'flarelink@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 # debug mode to easily check new website
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm  # a package that allows for easy form creation
+from wtforms import StringField, PasswordField, SubmitField, BooleanField  # gets string field for person to enter in
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
