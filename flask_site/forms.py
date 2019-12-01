33d537227527e9fe4fdea231848f6220b73c2caf
hxs7174 <hxs7174@g.rit.edu>
2019-11-30 23:52:35 -0500

    create, update, and delete posts

--- flask_site/forms.py
+++ flask_site/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm  # a package that allows for easy form creation
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField  # gets string field for person to enter in
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_site.db_models import User
 
@@ -54,3 +54,9 @@ class UpdateAccountForm(FlaskForm):
             email = User.query.filter_by(email=email.data).first()
             if email:
                 raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
