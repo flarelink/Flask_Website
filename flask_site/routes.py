c2948d530766fb6914e50a70511b7b5cd3d4ec0c
hxs7174 <hxs7174@g.rit.edu>
2019-12-01 00:28:11 -0500

    pagination

--- flask_site/routes.py
+++ flask_site/routes.py
@@ -15,7 +15,8 @@ db.create_all()
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', title='Home', posts=posts)
 
 
@@ -165,3 +166,15 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+# specific user's posts
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query\
+        .filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', title=user.username + "'s posts", posts=posts, user=user)
